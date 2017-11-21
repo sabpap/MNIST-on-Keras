3d9017a5dc66ac9a87c44131ef9065dee1b1e0b2
Unknown <sabpap@hotmail.com>
2017-11-21 02:09:22 +0200

    v2
    -evaluation added
    -comments added

--- Chtr_Recogn_on_Keras.py
+++ Chtr_Recogn_on_Keras.py
@@ -40,8 +40,7 @@ if K.image_data_format() == 'channels_first':
 else:
     x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
     x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    
-input_shape = (img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
 
 x_train = x_train.astype('float32') #convert to float for division
 x_test = x_test.astype('float32') #convert to float for division
--- Load&useModel.py
+++ Load&useModel.py
@@ -9,33 +9,61 @@ Created on Sun Nov 19 19:33:49 2017
 
 @author: sabpap
 """
-
+#import libraries
 import keras.utils 
 from keras.models import model_from_json
 import numpy as np
 from scipy.misc import imread, imresize
+from keras.datasets import mnist
+
+from keras import backend as K
 
 
-
-img_rows, img_cols = 28,28 #input images dimensions
-
-
-# load json and create model
+# load model 
 json_file = open('model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
+
 # load weights into new model
 loaded_model.load_weights("model.h5")
 print("Loaded model from disk")
  
-# evaluate loaded model on test data
+# compile model
 loaded_model.compile(loss= keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(), metrics = ['accuracy'])
 
+# load data for evaluation
+img_rows, img_cols = 28,28 #input images dimensions
+num_classes = 10 #10 different characters
 
+(x_train, y_train), (x_test, y_test) = mnist.load_data() #loading data
+
+
+#data preprocessing
+
+#For 3D data, "channels_last" assumes (conv_dim1, conv_dim2, conv_dim3, channels) while 
+#"channels_first" assumes (channels, conv_dim1, conv_dim2, conv_dim3).
+if K.image_data_format() == 'channels_first':
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+x_test = x_test.astype('float32') #convert to float for division
+x_test /= 255   #normalize
+
+print("%d test samples\n" % x_test.shape[0])
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+#evaluate on test data
+print("evaluating...")
+score = loaded_model.evaluate(x_test, y_test, verbose=0)
+print("%s: %.2f%%\n" % (loaded_model.metrics_names[1], score[1]*100))
 
 #testing/playing with new images
 
+#image 1
 one = imread('one.png',mode='L')
 #imshow(one)
 #one.astype('float32')
@@ -49,7 +77,7 @@ one = one.reshape(1,img_rows, img_cols,1)
 x = loaded_model.predict_proba(one)
 x = x.astype(int)
 
-
+#image2
 four = imread('two.png',mode='L')
 #imshow(one)
 #one.astype('float32')
